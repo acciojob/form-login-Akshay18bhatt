691ea60741368604cad153ecf436664ed1950890
Akshay Bhatt <120771512+Akshay18bhatt@users.noreply.github.com>
2023-11-21 16:03:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
 function getFormvalue() {
     //Write your code here
+	let list= document.querySelectorAll("input");
+	console.log(list);
+	let fname=list[0].value;
+	let lname=list[1].value;
+	let result= fname+" "+lname;
+	// console.log(fname);
+	// console.log(lname);
+	// console.log(result);
+	alert(result);
 
 }
